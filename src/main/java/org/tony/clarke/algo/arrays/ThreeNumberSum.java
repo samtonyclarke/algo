e83ad5ec3ec8b7e10cde6e58a243cee28f0c9bf9
Tony Clarke <tony.clarke@amdocs.com>
2023-02-21 14:05:41 -0500

    refactor

--- src/main/java/org/tony/clarke/algo/arrays/ThreeNumberSum.java
+++ src/main/java/org/tony/clarke/algo/arrays/ThreeNumberSum.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.tony.clarke.algo.arrays;
 
 import java.util.*;
 
--- src/main/java/org/tony/clarke/algo/bst/BstConstruction.java
+++ src/main/java/org/tony/clarke/algo/bst/BstConstruction.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.tony.clarke.algo.bst;
 
 
 public class BstConstruction {
